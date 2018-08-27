b523a6744e45fe25ad1a6d8bc33abdb4f04b25e7
Steven Peng <pentwo@gmail.com>
2018-08-27 21:29:28 +0800

    tidy-up code

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,3 @@
-const tableNorth = document.querySelector(".tableNorth");
-const tableSouth = document.querySelector(".tableSouth");
-const tableRockMan = document.querySelector(".tableRockMan");
 const table = document.querySelector(".table");
 
 const tableTitle = `
@@ -14,84 +11,15 @@ const tableTitle = `
   <tbody>
   `;
 
-// let northHtml = tableTitle;
-// let southHtml = tableTitle;
-// let rockManHtml = tableTitle;
-// let html = tableTitle;
-
-// function showTab(area) {
-//   for (const item of document.querySelectorAll(".table")) {
-//     item.setAttribute("style", "display:none;");
-//   }
-//   document
-//     .querySelector(`.table${area}`)
-//     .setAttribute("style", "display:inherit");
-// }
-// function setTabAct(area) {
-//   for (const item of document.querySelectorAll(".list")) {
-//     item.classList.remove("is-active");
-//   }
-//   document.querySelector(`.list-${area}`).classList.add("is-active");
-// }
-
 function showSouth() {
-  // showTab("South");
-  // setTabAct("south");
   fetchData("South");
 }
 function showNorth() {
-  // showTab("North");
-  // setTabAct("north");
   fetchData("North");
 }
 function showRockMan() {
-  // showTab("RockMan");
-  // setTabAct("rock");
   fetchData("Rockingham/Mandurah");
 }
-
-// const data = fetch("suburbs.json")
-//   .then(data => data.json())
-//   .then(data => {
-//     return data.forEach(item => {
-//       if (item.Area === "North") {
-//         northHtml += `
-//           <tbody>
-//             <tr>
-//               <td>${item.Suburb}</td>
-//               <td>${item.Houses}</td>
-//               <td><a href="${item.Links}" target="_blank">Map</a></td>
-//             </tr>
-//           </tbody>
-//         `;
-//         tableNorth.innerHTML = northHtml;
-//       }
-//       if (item.Area === "South") {
-//         southHtml += `
-//           <tbody>
-//             <tr>
-//               <td>${item.Suburb}</td>
-//               <td>${item.Houses}</td>
-//               <td><a href="${item.Links}" target="_blank">Map</a></td>
-//             </tr>
-//           </tbody>
-//         `;
-//         tableSouth.innerHTML = southHtml;
-//       }
-//       if (item.Area === "Rockingham/Mandurah") {
-//         rockManHtml += `
-//           <tbody>
-//             <tr>
-//               <td>${Capitalize(item.Suburb.toLowerCase())}</td>
-//               <td>${item.Houses}</td>
-//               <td><a href="${item.Links}" target="_blank">Map</a></td>
-//             </tr>
-//           </tbody>
-//         `;
-//         tableRockMan.innerHTML = rockManHtml;
-//       }
-//     });
-//   });
 
 function fetchData(prop) {
   fetch("suburbs.json")
